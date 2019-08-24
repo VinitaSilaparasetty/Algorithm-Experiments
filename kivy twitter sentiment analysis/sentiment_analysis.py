afdc95d1a9e8a464b77843a15f820c2afd7cb8d4
Vinita Silaparasetty <vinitasilaparasetty@gmail.com>
2019-08-24 23:29:29 +0530

    Update and rename sentiment_analysis1.py to sentiment_analysis.py

--- kivy twitter sentiment analysis/sentiment_analysis.py
+++ kivy twitter sentiment analysis/sentiment_analysis.py
@@ -74,3 +74,15 @@ elif(polarity>0.00):
     print("Positively")
 
 #LoopInTextBlob
+
+labels=["Positive["+str(positive)+"%]","Mixed Views["+str(mixed)+"%]","Negative["+str(negative)+"%]"]
+sizes=[positive,mixed,negative]
+colors=["pink","lightgreen","lightblue"]
+patches,texts=plt.pie(sizes,colors=colors,startangle=90)
+plt.legend(patches,labels,loc="best")
+plt.title("People are reacting on"+searchTerm)
+plt.axis("equal")
+plt.tight_layout()
+plt.show()
+
+#PlottingThePieChart
