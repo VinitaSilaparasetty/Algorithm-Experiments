ef91faf3d04242942b9852ed93d7aed4fdffa4e9
Vinita Silaparasetty <vinitasilaparasetty@gmail.com>
2019-08-25 15:08:45 +0530

    Update sentiment_analysis.py

--- kivy twitter sentiment analysis/sentiment_analysis.py
+++ kivy twitter sentiment analysis/sentiment_analysis.py
@@ -1,41 +1,36 @@
-#Import Statements#
+#Import Libraries
 
 from textblob import TextBlob  	 #ForAnalyzingGrammarInData 
 import sys, tweepy            	 #ForAuthentication
 import matplotlib.pyplot as plt 	 #ForPlottingPieChart
 
 
-
+#Display percentage values of data
 def percentage(part,whole):
     return 100*float(part)/float(whole) 
 
-#ToShowDataInPercentage
 
-
-
+#Secret keys for tweepy
 consumerKey="Kx05pI2ZHtRkQs6LHsLYOkeRK"
 consumerSecret="safrrew"
 accessToken="ss-agsdga"
 accessTokenSecret="slfjas;ojsjf" 
- #PasscodesForAuthentication
+ 
 
-
-searchTerm=input("Enter hashtag to search:")
-numberofSeachTerms=int(input("How many tweets to analyze?")) 
-
-#FirstInput
+#Retrieving Data
+searchTerm=input("Enter Desired Hashtag: ")
+numberofSeachTerms=int(input("Number of Tweets to Analyze :")) 
 
 tweets=tweepy.Cursor(api.search, q=searchTerm, lang="English").items(numberofSeachTerms)
 
-#GetData
-
+#initialize counters to 0
 positive=0.00
 negative=0.00
 mixed=0.00
 polarity=0.00
 
-#DefineSegregation
 
+#Categorization
 for tweet in tweets:
     print(tweet.text) 		#PrintingRecievedData
     analysis=TextBlob(tweet.text)	#DeclareLoopConditions
@@ -47,24 +42,24 @@ for tweet in tweets:
     elif(analysis.sentiment.polarity>0.00):
         positive+=1
 
-#LoopEnds
 
+#Call Percentage Function
 positive=percentage(positive,numberofSeachTerms)
 negative=percentage(negative,numberofSeachTerms)
 mixed=percentage(mixed,numberofSeachTerms)
 polarity=percentage(polarity,numberofSeachTerms)
 
-#CallingPercentageFunction
 
+#Converting To Float Valuess
 
 positive=format(positive,'.2f')
 negative=format(negative,'.2f')
 mixed=format(mixed,'.2f')
 
-#ConvertingToFloatVals
 
 
-print('How people are reacting on'+searchTerm)
+
+print('People's Reactions to'+searchTerm)
 
 if(polarity==0):
     print("Mixed Views")
@@ -73,7 +68,7 @@ elif(polarity<0.00):
 elif(polarity>0.00):
     print("Positively")
 
-#LoopInTextBlob
+#Plotting the PieChart
 
 labels=["Positive["+str(positive)+"%]","Mixed Views["+str(mixed)+"%]","Negative["+str(negative)+"%]"]
 sizes=[positive,mixed,negative]
@@ -85,4 +80,4 @@ plt.axis("equal")
 plt.tight_layout()
 plt.show()
 
-#PlottingThePieChart
+
