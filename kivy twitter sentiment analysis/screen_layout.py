bede42822110651a3e510e17b434737295d5b250
Vinita Silaparasetty <vinitasilaparasetty@gmail.com>
2019-08-25 11:45:43 +0530

    Update screen_layout.py

--- kivy twitter sentiment analysis/screen_layout.py
+++ kivy twitter sentiment analysis/screen_layout.py
@@ -1,3 +1,4 @@
+#import libraries
 from textblob import TextBlob
 import sys, tweepy
 import matplotlib.pyplot as plt
@@ -6,27 +7,29 @@ import matplotlib.pyplot as plt
 def percentage(part,whole):
     return 100*float(part)/float(whole)
 
-consumerKey="Kx05pI2ZHtRkQs6LHsLYOkeRK"
-consumerSecret="grY1yujbtYBRwfspIS8swiFoXUlTrBgr5I4IwcQhBbWmyFgYPM"
-accessToken="2530152451-OO6vRCLjNXQIaHyelo1TdFhSlI8YpBzpUX91RAa"
-accessTokenSecret="jscWVipPNASXspdVbwNxiRY1ZpiAw3syUi6agB4kcmr1m"
+#secret keys for tweepy
+consumerKey="***"
+consumerSecret="***"
+accessToken="***"
+accessTokenSecret="***"
 
 
 auth=tweepy.OAuthHandler(consumer_key=consumerKey,consumer_secret=consumerSecret)
 auth.set_access_token(accessToken,accessTokenSecret)
 api=tweepy.API(auth)
 
-searchTerm=input("Enter hashtag to search:")
-numberofSeachTerms=int(input("How many tweets to analyze?"))
+searchTerm=input("Enter Desired Hashtag: ")
+numberofSeachTerms=int(input("Number of Tweets to Analyze :"))
 
 tweets=tweepy.Cursor(api.search, q=searchTerm, lang="English").items(numberofSeachTerms)
 
-
+#initialize counters to 0
 positive=0.00
 negative=0.00
 mixed=0.00
 polarity=0.00
 
+#sentiment analysis
 for tweet in tweets:
     print(tweet.text)
     analysis=TextBlob(tweet.text)
@@ -47,7 +50,9 @@ positive=format(positive,'.2f')
 negative=format(negative,'.2f')
 mixed=format(mixed,'.2f')
 
-print('How people are reacting on'+searchTerm)
+print('People's Reactions to'+searchTerm)
+
+#categorize reactions
 
 if(polarity==0):
     print("Mixed Views")
@@ -58,10 +63,10 @@ elif(polarity>0.00):
 
 labels=["Positive["+str(positive)+"%]","Mixed Views["+str(mixed)+"%]","Negative["+str(negative)+"%]"]
 sizes=[positive,mixed,negative]
-colors=["pink","lightgreen","lightblue"]
+colors=["darkblue","purple","lightblue"]
 patches,texts=plt.pie(sizes,colors=colors,startangle=90)
 plt.legend(patches,labels,loc="best")
-plt.title("People are reacting on"+searchTerm)
+plt.title("People's Reactions to"+searchTerm)
 plt.axis("equal")
 plt.tight_layout()
 plt.show()
